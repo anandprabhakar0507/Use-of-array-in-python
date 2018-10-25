1c457085f1a5e74dcaffdb7384d6adbd17ade614
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-24 18:27:18 -0700

    add files to uploads..

---
+++ array.py
@@ -0,0 +1,13 @@
+﻿#! /usr/bin/env python3
+
+import turtle
+
+print "GO RUN YOUR CODE FRIENDS..." 
+print ('Note:Be careful of indentation mistakes while writing python scripts...-ANAND PRABHAkAR')
+        
+import turtle
+t = turtle.Turtle()
+for c in ['red', 'green', 'yellow', 'blue', 'brown', 'violet', 'indigo', 'blue', 'lightgreen', 'yellow', 'orangre', 'red', 'pink', 'skyblue', 'brown', 'violet', 'indigo', 'blue', 'lightgreen', 'yellow', 'orangre', 'red', 'pink', 'skyblue']:
+    t.color(c)
+    t.forward(75)
+    t.left(90)
